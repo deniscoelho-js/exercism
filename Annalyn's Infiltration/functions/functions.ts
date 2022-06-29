f1b82a60643282d03ad7f2f3136e6c4fb4547e7f
Denis Coelho <deniscoelho.dev@gmail.com>
2022-06-28 22:51:58 -0300

    atualizacao

--- Annalyn's Infiltration/functions/functions.ts
+++ Annalyn's Infiltration/functions/functions.ts
@@ -9,7 +9,7 @@ export const isBoolean = (): boolean => {
 };
 
 export const canExecuteFastAttack = (knightIsAwake: boolean) => {
-  if (knightIsAwake === true) {
+  if (knightIsAwake) {
     return console.log(
       "Não é possivel fazer um ataque rápido, pois o cavaleiro está acordado. "
     );
@@ -18,4 +18,60 @@ export const canExecuteFastAttack = (knightIsAwake: boolean) => {
       "Ataque rápido executado com sucesso! Cavaleiro está dormindo "
     );
   }
+};
+
+export const canSpy = (
+  knightIsAwake: boolean,
+  archerIsAwake: boolean,
+  prisonerIsAwake: boolean
+) => {
+  if (
+    knightIsAwake === false &&
+    archerIsAwake === true &&
+    prisonerIsAwake === false
+  ) {
+    return console.log("É possível espiar!");
+  } else {
+    return console.log("Não é possível espiar!");
+  }
+};
+
+export const canSignalPrisoner = (
+  archerIsAwake: boolean,
+  prisonerIsAwake: boolean
+) => {
+  if (archerIsAwake === false && prisonerIsAwake === true) {
+    return console.log("É possível dar um sinal para o prisioneiro!");
+  } else {
+    return console.log("Não é possível dar um sinal para o prisionneiro!");
+  }
+};
+
+export const canFreePrisoner = (
+  knightIsAwake: boolean,
+  archerIsAwake: boolean,
+  prisonerIsAwake: boolean,
+  petDogIsPresent: boolean
+) => {
+  if (
+    knightIsAwake &&
+    archerIsAwake === false &&
+    prisonerIsAwake &&
+    petDogIsPresent === true
+  ) {
+    return console.log("Não foi possível libertar o prisioneiro! ");
+  } else {
+    return console.log("Show! O prisioneiro está livre. ");
+  }
+};
+
+export const characterStates = (
+  knightIsAwake: boolean,
+  archerIsAwake: boolean,
+  prisonerIsAwake: boolean,
+  petDogIsPresent: boolean
+) => {
+  return console.log(
+    `Cavaleiro:${knightIsAwake} | Arqueiro:${archerIsAwake} | Prisioneiro:${prisonerIsAwake} | Cachorro:${petDogIsPresent} `
+  );
 };
--- Annalyn's Infiltration/index.ts
+++ Annalyn's Infiltration/index.ts
@@ -1,16 +1,29 @@
 import prompt from "prompt-sync";
-import { canExecuteFastAttack, isBoolean } from "./functions/functions";
+import {
+  canExecuteFastAttack,
+  isBoolean,
+  canSpy,
+  canFreePrisoner,
+  canSignalPrisoner,
+  characterStates,
+} from "./functions/functions";
 
 console.clear();
 
 const readline = prompt();
 
-let knightIsAwake: boolean = true;
-let archerIsAwake: boolean = true;
-let prisonerIsAwake: boolean = true;
-let petDogIsPresent: boolean = true;
+let knightIsAwake: boolean;
+let archerIsAwake: boolean;
+let prisonerIsAwake: boolean;
+let petDogIsPresent: boolean;
 
 prisonerIsAwake = isBoolean();
 archerIsAwake = isBoolean();
 knightIsAwake = isBoolean();
 petDogIsPresent = isBoolean();
+
+characterStates(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent);
+canExecuteFastAttack(knightIsAwake);
+canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake);
+canSignalPrisoner(archerIsAwake, prisonerIsAwake);
+canFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent);
